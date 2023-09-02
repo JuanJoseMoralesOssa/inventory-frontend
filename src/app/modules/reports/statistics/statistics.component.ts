ae766c57be3872c41cfaf9bed1cc2f7ed36ebe66
juan <juanjomoss@gmail.com>
2023-09-02 18:10:00 -0500

    Se agrego la opcion de filtrar las ventas desde dos input

--- src/app/modules/reports/statistics/statistics.component.ts
+++ src/app/modules/reports/statistics/statistics.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./statistics.component.css']
 })
 export class StatisticsComponent {
+  startDate: string = '';
+  endDate: string = '';
+  resultArr: any[] = [];
+  result: string = '';
 
+  constructor(private http: HttpClient) {}
+
+  consultFilteredSales() {
+    // Realiza una llamada al backend con las fechas seleccionadas
+    const filter = {
+      startDate: this.startDate,
+      endDate: this.endDate
+    };
+
+    this.http.post<any>('http://localhost:3000/sale-filtered', filter).subscribe({
+      next: data => {
+        // Procesa la respuesta del backend y asigna el resultado
+        this.result = JSON.stringify(data);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
+
+
+  copyToClipboard() {
+    // Lógica para copiar this.result al portapapeles
+    const el = document.createElement('textarea');
+    el.value = this.result;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+  }
 }
